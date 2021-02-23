befe97fff61d10e429dad27c2518ebbeeb2b75f7
Inghtd <61320222+Inghtd@users.noreply.github.com>
2021-02-23 09:35:29 -0600

    Update index.php

--- sistema/index.php
+++ sistema/index.php
@@ -3,6 +3,7 @@
 //error_reporting(E_ALL);
 //echo "1";
 //Incluimos el FrontController
+header('Strict-Transport-Security: max-age=0;');
 require '../class/controlador.php';
 //Lo iniciamos con su método estático main.
 FrontController::main();
